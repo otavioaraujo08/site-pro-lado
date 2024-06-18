34a558a8ddabb389cc478922e79673288880361c
otavioaraujo08 <otavioaraujo490@gmail.com>
2024-06-18 17:30:00 -0300

    feat: add database of loved things by my girlfriend

---
+++ src/components/lovedThingsData.js
@@ -0,0 +1,45 @@
+import bananaFishImage from "../images/bananaFish.png";
+import coxinha from "../images/coxinha.png";
+import vanGogh from "../images/vanGogh.png";
+import cafe from "../images/cafe.png";
+import zed from "../images/zed.png";
+import caracol from "../images/caracol.png";
+import zedinho from "../images/zedinho.png";
+
+export const lovedThingsData = [
+  {
+    id: 0,
+    name: "Banana Fish",
+    imageSrc: bananaFishImage,
+  },
+  {
+    id: 1,
+    name: "Coxinha",
+    imageSrc: coxinha,
+  },
+  {
+    id: 2,
+    name: "Van Gogh",
+    imageSrc: vanGogh,
+  },
+  {
+    id: 3,
+    name: "Cafe",
+    imageSrc: cafe,
+  },
+  {
+    id: 4,
+    name: "Zed",
+    imageSrc: zed,
+  },
+  {
+    id: 5,
+    name: "Euzinho",
+    imageSrc: caracol,
+  },
+  {
+    id: 6,
+    name: "Eu ja falei do zed ?",
+    imageSrc: zedinho,
+  },
+];
