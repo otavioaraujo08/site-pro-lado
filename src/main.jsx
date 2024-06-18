c2f6335b674368102202c2853b5b4b86dcdc0e47
otavioaraujo08 <otavioaraujo490@gmail.com>
2024-06-18 17:31:29 -0300

    feat: update home page

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { LovedThings } from "./components/lovedThings/index";
+import { lovedThingsData } from "./components/lovedThingsData";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <body>hello world</body>
+    <div className="mainDiv">
+      <div className="mainHeader">
+        <h1>Coisas que minha namorada ama ❤️</h1>
+      </div>
+
+      <div className="mainBody">
+        {lovedThingsData.map((item) => (
+          <div key={item.id} className="dataDiv">
+            <LovedThings title={item.name} image={item.imageSrc} />
+          </div>
+        ))}
+      </div>
+    </div>
   </React.StrictMode>
 );
