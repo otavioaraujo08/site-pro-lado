a6430a65a884448eecd9112415be3d8753ce73d8
otavioaraujo08 <otavioaraujo490@gmail.com>
2024-06-21 19:17:24 -0300

    feat: add timeTogether component to main page and fix classname title to another with more content

--- src/main.jsx
+++ src/main.jsx
@@ -2,21 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { LovedThings } from "./components/lovedThings/index";
-import { lovedThingsData } from "./components/lovedThingsData";
+import { lovedThingsData } from "./components/lovedThings/lovedThingsData";
+import { TimeTogether } from "./components/timeTogether";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <div className="mainDiv">
-      <div className="mainHeader">
-        <h1>Coisas que minha namorada ama ❤️</h1>
-      </div>
+    <div>
+      <TimeTogether />
 
-      <div className="mainBody">
-        {lovedThingsData.map((item) => (
-          <div key={item.id} className="dataDiv">
-            <LovedThings title={item.name} image={item.imageSrc} />
-          </div>
-        ))}
+      <div className="lovedContainer">
+        <div className="lovedHeader">
+          <h1>Coisas que minha namorada ama ❤️</h1>
+        </div>
+
+        <div className="lovedBody">
+          {lovedThingsData.map((item) => (
+            <div key={item.id} className="dataDiv">
+              <LovedThings title={item.name} image={item.imageSrc} />
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   </React.StrictMode>
